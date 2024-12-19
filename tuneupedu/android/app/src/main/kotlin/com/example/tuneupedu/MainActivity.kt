91058a95b3fc89499b79afcb13fa5da2f9d0f085
sairam-jeeva <sairamjeeva18@gmail.com>
2024-12-19 15:57:17 +0530

    BREAKING CHANGES: Firebase Connected

--- tuneupedu/android/app/src/main/kotlin/com/example/tuneupedu/MainActivity.kt
+++ tuneupedu/android/app/src/main/kotlin/com/example/tuneupedu/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.tuneupedu
+package com.tuneup.edu
 
 import io.flutter.embedding.android.FlutterActivity
 
--- tuneupedu/lib/main.dart
+++ tuneupedu/lib/main.dart
@@ -1,6 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:tuneupedu/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -31,7 +37,6 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
